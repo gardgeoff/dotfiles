796ed03ef2dce14777046f3c4cecb99770431a35
Geoff Gard <gardgeoff@gmail.com>
2023-12-25 16:01:44 -0700

    update catpuccin theme

--- nvim/lua/user/plugins/catpuccin.lua
+++ nvim/lua/user/plugins/catpuccin.lua
@@ -1,9 +1,27 @@
 return {
     'catppuccin/nvim',
     name = "catppuccin",
+    opts = {
+      integrations = {
+        telescope = {
+          enabled = true,
+          style = "nvchad",
+        }
+      },
+      custom_highlights = function(colors)
+        local prompt = "#2d3149"
+        return {
+          FloatermBorder = {
+            bg = prompt,
+            fg = prompt,
+          },
+        }
+      end
+    },
     priority = 1000,
     config = function (plugin, opts)
-        require('catppuccin').setup()
+        require('catppuccin').setup(opts)
         vim.cmd('colorscheme catppuccin-mocha')
     end,
 }
+
