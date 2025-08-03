2178ca3cf2081e762bfad28df3c1cd54b2f927b2
Geoff Gard <gardgeoff@gmail.com>
2025-08-03 10:29:31 -0700

    Update keymaps for php class imports

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -52,9 +52,4 @@ vim.keymap.set('n', '<C-s>', ':up <CR>')
 vim.keymap.set('n', '<C-w>', ':bd <CR>')
 
 -- Move text up and down
--- vim.keymap.set('i', '<A-j>', '<Esc>:move .+1<CR>==gi')
--- vim.keymap.set('i', '<A-k>', '<Esc>:move .-2<CR>==gi')
--- vim.keymap.set('n', '<A-j>', ':move .+1<CR>==')
--- vim.keymap.set('n', '<A-k>', ':move .-2<CR>==')
--- vim.keymap.set('v', '<A-j>', ":move '>+1<CR>gv=gv")
--- vim.keymap.set('v', '<A-k>', ":move '<-2<CR>gv=gv")
+vim.keymap.set('n', '<C-i>', ':PhpactorImportClass <CR>')
