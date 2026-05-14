cb60bd63fe715e89de81041f213943be71146b1d
Geoff Gard <gardgeoff@gmail.com>
2026-05-13 20:47:10 -0700

    make treesitter compatible with main

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,11 @@
+
 require('user.options')
 require('user.keymaps')
 require('user.misc')
 require('user.plugins')
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'php' },
+  callback = function()
+    vim.treesitter.start()
+  end,
+})
